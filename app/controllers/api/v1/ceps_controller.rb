fa43d804757417dc4022ed494ffccb46cddb9e60
Julia Guerreiro <juguerreiro123@gmail.com>
2022-09-30 10:11:11 -0300

    erase the binding pry

--- app/controllers/api/v1/ceps_controller.rb
+++ app/controllers/api/v1/ceps_controller.rb
@@ -1,10 +1,8 @@
 module Api
   module V1
     class CepsController < Api::V1::BaseController
-      require "pry-byebug"
       acts_as_token_authentication_handler_for User
       def index
-        binding.pry
         @ceps = policy_scope(Cep)
       end
 
